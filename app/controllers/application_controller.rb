cb4bbdd5b8766d1a5feda4ee9b65e5027ad0e5da
Viral Jain <viral.jain@sigmainfo.net>
2024-01-25 10:10:52 +0530

    delete some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, unless: :pages_controller?
-
-  private
-  def pages_controller?
-    controller_name == 'pages' && action_name == 'home'
-  end
-  
-  def authenticate_user!
-    unless user_signed_in?
-      redirect_to root_path, alert: "You must be signed in to access this page."
-    end
-  end
 end
